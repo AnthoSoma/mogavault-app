20326732f3fc908bbbda62a7699dc0c38ee39b8e
Arkanos <anthony.pierron54@gmail.com>
2026-09-22 12:38:56 +0200

    main: wip

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmButton } from '@spartan-ng/helm/button';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmButton],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
