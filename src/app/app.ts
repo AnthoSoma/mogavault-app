340753e89dcacf48280e217cbc51855380e6e3eb
Arkanos <anthony.pierron54@gmail.com>
2026-09-24 12:02:08 +0200

    feat(feature/login): wip

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HlmButton } from '@spartan-ng/helm/button';
 
 @Component({
-  imports: [RouterOutlet, HlmButton],
+  imports: [RouterOutlet],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
